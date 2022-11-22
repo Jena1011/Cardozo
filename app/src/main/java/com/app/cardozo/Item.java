b00cf840661fcb7393b8c5126000d53b48c0f1c3
jena <ttps860823@gmail.com>
2022-11-23 01:00:32 +0800

    1. 拉基本介面:只有UI元件 登入、登出、註冊、商品列表(含卡片)、商品、購物車(含卡片)

---
+++ app/src/main/java/com/app/cardozo/Item.java
@@ -0,0 +1,14 @@
+package com.app.cardozo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+    }
+}
